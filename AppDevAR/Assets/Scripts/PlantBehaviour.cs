8bf2077c31ad3b1966bc37834167db5a9b4a2f2a
billydragon17 <130969160+billydragon17@users.noreply.github.com>
2023-07-25 15:33:29 +0200

    Slider Behaviour

---
+++ AppDevAR/Assets/Scripts/PlantBehaviour.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider slider;
+    public GameObject[] objects;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeFlowerState();
+    }
+
+    public void ChangeFlowerState()
+    {
+        if (slider.value >= 5f)
+        {
+            SetOthersInactive();
+            objects[1].SetActive(true);
+        }
+        else
+        {
+            SetOthersInactive();
+            objects[0].SetActive(true);
+        }
+    }
+
+    public void SetOthersInactive()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i].SetActive(false);
+        }
+    }
+}
