377283458eb8a589a1399f0fa16e51bec14fc74c
LongNguyen17 <130969160+billydragon17@users.noreply.github.com>
2023-07-26 15:11:33 +0200

    Modified Sunflower Slider

--- AppDevAR/Assets/Scripts/SunflowerPlantBehaviour.cs
+++ AppDevAR/Assets/Scripts/SunflowerPlantBehaviour.cs
@@ -27,27 +27,27 @@ public class SunflowerBehaviourScript : MonoBehaviour
     {
         switch (slider.value)
         {
+            case (3f):
+                SetOthersInactive();
+                objects[0].SetActive(true);
+                return;
             case (4f):
                 SetOthersInactive();
                 objects[0].SetActive(true);
                 return;
-            case (5f):
-                SetOthersInactive();
-                objects[0].SetActive(true);
-                return;
-            case (7f):
+            case (6f):
                 SetOthersInactive();
                 objects[1].SetActive(true);
                 return;
-            case (8f):
+            case (7f):
                 SetOthersInactive();
                 objects[2].SetActive(true);
                 return;
-            case (9f):
+            case (8f):
                 SetOthersInactive();
                 objects[3].SetActive(true);
                 return;
-            case (10f):
+            case (9f):
                 SetOthersInactive();
                 objects[4].SetActive(true);
                 return;
